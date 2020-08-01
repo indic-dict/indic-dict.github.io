6c999c6407cfcefe5b6b3c1c7bb8e75b36700851
vvasuki <vishvas.vasuki@gmail.com>
2020-08-01 18:08:39 +0530

    CU

--- themes/shabdakosha/webpack_src/js/main.js
+++ themes/shabdakosha/webpack_src/js/main.js
@@ -15,6 +15,15 @@ function loadShabdaDetailsHandler() {
     window.location = baseURL + `/shabda-details?shabda=${shabda}`;
 }
 
+function setCanonicalUrl() {
+    // Set canonical URL so that Google does not exclude this page from the index. See https://github.com/ashtadhyayi/ashtadhyayi.github.io/issues/11 .
+    var link = !!document.querySelector("link[rel='canonical']") ? document.querySelector("link[rel='canonical']") : document.createElement('link');
+    link.setAttribute('rel', 'canonical');
+    link.setAttribute('href', location.protocol + '//' + location.host + "/shabda-details/?shabda=" + shabdaId);
+    document.head.appendChild(link);
+}
+
+
 import handleIncludes from "./handleIncludes";
 
 $( document ).ready(function() {
@@ -24,6 +33,7 @@ $( document ).ready(function() {
             shabdaId = getQueryVariable("shabda");
         }
     }
+    setCanonicalUrl();
     document.getElementById("shabdaSearchInputBox").value = shabdaId
     console.log(shabdaId);
     $("#shabdaSearchInputBox").change(loadShabdaDetailsHandler);
